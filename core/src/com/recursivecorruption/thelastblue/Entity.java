4f8d1deed019a23b7fd4477e3b8f577df4700880
Wolfgange3311999 <matthew3311999@gmail.com>
2016-05-10 22:13:38 -0500

    Particles now diffuse more realistically

--- core/src/com/recursivecorruption/thelastblue/Entity.java
+++ core/src/com/recursivecorruption/thelastblue/Entity.java
@@ -38,7 +38,7 @@ public abstract class Entity {
         List<Entity> particles = new ArrayList<Entity>();
         for (float x = pos.x; x <= pos.x + radius; x += Particle.PARTICLE_SIZE)
             for (float y = pos.y; y <= pos.y + radius; y += Particle.PARTICLE_SIZE)
-                particles.add(new Particle(x, y, vel.x * 0.3f + 60f * (rand.nextFloat() - 0.5f), vel.y * 0.3f + 60f * (rand.nextFloat() - 0.5f), color, Particle.PARTICLE_SIZE, noFade));
+                particles.add(new Particle(x, y, vel.x * 0.3f + 1.4f * radius * (rand.nextFloat() - 0.5f), vel.y * 0.3f + 1.4f * radius * (rand.nextFloat() - 0.5f), color, Particle.PARTICLE_SIZE, noFade));
         return particles;
     }
 
