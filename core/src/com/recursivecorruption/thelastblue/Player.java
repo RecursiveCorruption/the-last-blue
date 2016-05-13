e374891ec1e920876205a77000da1e0e879431be
Wolfgange3311999 <matthew3311999@gmail.com>
2016-05-12 21:05:27 -0500

    Refactored some code into handleInput function

--- core/src/com/recursivecorruption/thelastblue/Player.java
+++ core/src/com/recursivecorruption/thelastblue/Player.java
@@ -5,9 +5,6 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.recursivecorruption.thelastblue.graphics.Graphics;
 
-import java.util.List;
-import java.util.Random;
-
 public class Player extends Entity {
     public final static float FRICTION = 0.99f;
     private final static float TOUCH_MULTIPLY = 4.0f;
@@ -16,8 +13,7 @@ public class Player extends Entity {
         super(new Color(1f, 0.1f, 0.1f, 1f), 30, x, y, 0, 0);
     }
 
-    @Override
-    public Entity update(World world) {
+    private void handleInput() {
         float accel = 40f, mult = 1f;
         if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
             mult = 0;
@@ -33,6 +29,11 @@ public class Player extends Entity {
             vel.x = vel.x * mult - accel;
         else if (Gdx.input.isTouched())
             vel.set(InputProcessor.getDelta().scl(TOUCH_MULTIPLY));
+    }
+
+    @Override
+    public Entity update(World world) {
+        handleInput();
         vel.scl(FRICTION);
         pos.add(vel.x * Gdx.graphics.getDeltaTime(), vel.y * Gdx.graphics.getDeltaTime());
         float oldX = pos.x, oldY = pos.y;
